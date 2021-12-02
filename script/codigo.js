be87eaeeec8b88e3f8fd4f54446317469eaf3e67
Natalia Escobar Ayala <natalia07_@hotmail.com.ar>
2021-12-02 17:38:24 -0300

    fix

--- script/codigo.js
+++
@@ -1,5 +0,0 @@
-// document.getElementsByClassName("material-icons").addEventListener("click", mostrar_menu);
-
-// function mostrar_menu(){
-//     document.getElementById("move-content").classList.toggle("move-container-all")
-// }
